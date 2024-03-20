39fd765787f38144c667c970a7a742f58601ff13
Guy Zamir <54972542+Guy-Zamir@users.noreply.github.com>
2024-03-20 16:36:52 +0200

    Update mast_simulation.py

--- mast_simulation.py
+++ mast_simulation.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 st.title('Mast Simulation')
 
 # Users parameters
-days_to_simulate = st.number_input('Days To Simulate', min_value=1, max_value=365*50, value=365*30, step=1)
-n_iteration = st.number_input('Number Of Iterations', min_value=1, max_value=10000, value=1, step=1)
-daily_probability_to_mast_on_taking_money = st.number_input('Daily Probability To Mast - Taking Money', min_value=0.0, max_value=5.0, value=0.01, step=0.01, format="%.2f")
+days_to_simulate = st.number_input('Days to Simulate', min_value=1, max_value=365*50, value=365*30, step=1)
+n_iteration = st.number_input('Number of Iterations', min_value=1, max_value=10000, value=1, step=1)
+daily_probability_to_mast_on_taking_money = st.number_input('Daily Probability to Mast - Taking Money', min_value=0.0, max_value=5.0, value=0.01, step=0.01, format="%.2f")
 daily_mast_not_taking_money = st.number_input('Average Daily Mast - Not Taking Money', min_value=0.0, max_value=10.0, value=2.0, step=1.0, format="%.2f")
-yearly_return_on_investment = st.slider('Yearly Return On Investment', min_value=0.0, max_value=0.20, value=0.05, step=0.01)
+yearly_return_on_investment = st.slider('Yearly Return on Investment', min_value=0.0, max_value=0.20, value=0.05, step=0.01)
 invest_the_money = st.checkbox('Invest the Money', value=True)
 
 # Initialize arrays to store results
@@ -31,18 +31,24 @@ for iteration in range(n_iteration):
 
     # Single simulation loop
     for day in range(days_to_simulate):
+
         # When taking the money
         if np.random.rand() < daily_probability_to_mast_on_taking_money:
             money_taking_balance -= EVERY_MAST_LOSS
         if invest_the_money:
             money_taking_balance += money_taking_balance * daily_interest_rate
-        all_taking_money_history[iteration, day] = money_taking_balance
 
         # When not taking the money
         not_taking_money_balance += int(daily_mast_not_taking_money) * EVERY_MAST_WIN
         decimal_part = daily_mast_not_taking_money - int(daily_mast_not_taking_money)
         if np.random.rand() < decimal_part:
             not_taking_money_balance += EVERY_MAST_WIN
+        if invest_the_money:
+            not_taking_money_balance += not_taking_money_balance * daily_interest_rate
+
+        # Adding the days to the results
+        all_taking_money_history[iteration, day] = money_taking_balance
+        all_not_taking_money_history[iteration, day] = not_taking_money_balance
 
 avg_taking_money_history = np.mean(all_taking_money_history, axis=0)
 avg_not_taking_money_history = np.mean(all_not_taking_money_history, axis=0)
